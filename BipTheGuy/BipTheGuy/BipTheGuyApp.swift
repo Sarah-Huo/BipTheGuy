3d2b376feba4ecb51649a797221c68512b3a28c1
Sarah-Huo <106994306+Sarah-Huo@users.noreply.github.com>
2024-05-21 11:49:29 -0600

    Add files via upload

---
+++ BipTheGuy/BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Huo, Sarah Z on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
